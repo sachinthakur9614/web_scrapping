7675907914ddfca7b5d3ae7a09c889f7828882bc
sachinthakur9614 <sachin.thakur@mca.christuniversity.in>
2020-04-06 02:35:47 +0530

    add

--- web_file.py
+++ web_file.py
@@ -9,13 +9,11 @@ f1  = open("web_data.csv", 'w')
 writer = csv.writer(f1)
 
 writer.writerow([
-  "title",
-  "mobile"
-  "website",
-  "state and city",
-  "email",
-
-  
+"title",
+"mobile"
+"website",
+"state and city",
+"email"
 ])
 
 
@@ -23,24 +21,47 @@ writer.writerow([
 def write_row(title,mobile,website,address_city,email):
     csv_row = []
     csv_row.append(title)
-
     csv_row.append(mobile)
     csv_row.append(website) 
     csv_row.append(address_city)
     csv_row.append(email)
+    # try:
+        
+    # except:
+    #     csv_row.append("")
+
+    # try:
+        
+    # except:
+    #     csv_row.append("")   
+
+    # try:
+        
+    # except:
+    #     csv_row.append("") 
+
+    # try:
+        
+    # except:
+    #     csv_row.append("")  
+    # try:
+        
+    # except:
+    #     csv_row.append("")  
     writer.writerow(csv_row)
 
 
 
 
 k =0
-for i in range(0,840,15):
+count = 0
+for i in range(0,840):
     k  = k + 15
     html = requests.get("https://www.houzz.in/professionals/searchDirectory?topicId=26728&query=Furniture+%26+Accessories&location=india&distance=0&sort=4&p="+str(k))
     doc = lxml.html.fromstring(html.content)
     new_pro = doc.xpath('//span[@itemprop="name"]/text()')
     link = doc.xpath('//a[@class="pro-card-carousel__slide"]/@href')
-    count = 0
+    total = count
     for j in link:
         count = count + 1
         inside_link =requests.get(j)
@@ -53,11 +74,10 @@ for i in range(0,840,15):
         email = doc1.xpath('//form[@class="hz-contact-pro-modal__form"]/text()')
         print(title,mobile,website,address_city,email)
         write_row(title,mobile,website,address_city,email)
-        print("total rows", count)
+        
 
-
-
-
+    total = count + total
+    print("total rows", total)
 
 
 
