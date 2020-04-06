49fea4f604e9718bc8ef09f339a1dd58573bd43d
sachinthakur9614 <sachin.thakur@mca.christuniversity.in>
2020-04-06 18:57:34 +0530

    add

--- web_file.py
+++ web_file.py
@@ -3,17 +3,17 @@ import csv
 import re
 import lxml.html
 from bs4 import BeautifulSoup
-f1  = open("web_data.csv", 'w')
+f1  = open("Furniture_and_Accessories4.csv", 'w')
 
 
 writer = csv.writer(f1)
 
 writer.writerow([
-"title",
-"mobile"
-"website",
+"company name",
+"mobile no",
+"website link",
 "state and city",
-"email"
+"email",
 ])
 
 
@@ -25,29 +25,7 @@ def write_row(title,mobile,website,address_city,email):
     csv_row.append(website) 
     csv_row.append(address_city)
     csv_row.append(email)
-    # try:
-        
-    # except:
-    #     csv_row.append("")
-
-    # try:
-        
-    # except:
-    #     csv_row.append("")   
-
-    # try:
-        
-    # except:
-    #     csv_row.append("") 
-
-    # try:
-        
-    # except:
-    #     csv_row.append("")  
-    # try:
-        
-    # except:
-    #     csv_row.append("")  
+    
     writer.writerow(csv_row)
 
 
@@ -55,13 +33,14 @@ def write_row(title,mobile,website,address_city,email):
 
 k =0
 count = 0
-for i in range(0,840):
-    k  = k + 15
-    html = requests.get("https://www.houzz.in/professionals/searchDirectory?topicId=26728&query=Furniture+%26+Accessories&location=india&distance=0&sort=4&p="+str(k))
+for i in range(0,4635):
+    
+    html = requests.get('https://www.houzz.in/professionals/searchDirectory?topicId=26728&query=Furniture+%26amp%3B+Accessories&location=India&distance=0&sort=4&p='+str(k))
     doc = lxml.html.fromstring(html.content)
     new_pro = doc.xpath('//span[@itemprop="name"]/text()')
     link = doc.xpath('//a[@class="pro-card-carousel__slide"]/@href')
     total = count
+    k  = k + 15
     for j in link:
         count = count + 1
         inside_link =requests.get(j)
@@ -71,13 +50,18 @@ for i in range(0,840):
         website = doc1.xpath('//a[@data-compid="Profile_Website"]/@href')
         address = doc1.xpath('//div[@class="profile-meta__val"]/text()')
         address_city = doc1.xpath('//div[@class="hz-profile-header__location"]/text()')
-        email = doc1.xpath('//form[@class="hz-contact-pro-modal__form"]/text()')
+        email = doc1.xpath('//span[@class="profile-meta__block"]/text()')
+        category = doc1.xpath('//a/text()')
+
+
         print(title,mobile,website,address_city,email)
         write_row(title,mobile,website,address_city,email)
+        print("count",i)
         
 
-    total = count + total
-    print("total rows", total)
+
+
+
 
 
 
